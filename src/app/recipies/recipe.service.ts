3fcff4d7fed223c479505b9262356f084efd9748
JBar0 <baranowski.jakub97@gmail.com>
2020-04-28 20:46:51 +0200

    Forms added

--- src/app/recipies/recipe.service.ts
+++ src/app/recipies/recipe.service.ts
@@ -8,6 +8,7 @@ import {Subject} from 'rxjs';
   providedIn: 'root'
 })
 export class RecipeService {
+  recipesChanged: Subject<RecipeModel[]>;
 
   private recipes: RecipeModel[] = [
     new RecipeModel(
@@ -19,8 +20,8 @@ export class RecipeService {
         new IngredientModel('French fries', 20)
       ]),
     new RecipeModel(
-      'A test',
-      'Description',
+      'A test2',
+      'Description2',
       'https://assets.bonappetit.com/photos/5d7296eec4af4d0008ad1263/16:9/w_1200,c_limit/Basically-Gojuchang-Chicken-Recipe-Wide.jpg',
       [
         new IngredientModel('Meat', 1),
@@ -29,6 +30,7 @@ export class RecipeService {
   ];
 
   constructor(private shoppingListService: ShoppingListService) {
+    this.recipesChanged = new Subject<RecipeModel[]>();
   }
 
   getRecipes() {
@@ -41,5 +43,20 @@ export class RecipeService {
 
   addIngredientsToShoppingList(ingredients: IngredientModel[]) {
     this.shoppingListService.addIngredients(ingredients);
+  }
+
+  addRecipe(recipe: RecipeModel) {
+    this.recipes.push(recipe);
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  updateRecipe(index: number, newRecipe: RecipeModel) {
+    this.recipes[index] = newRecipe;
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  deleteRecipe(index: number) {
+    this.recipes.splice(index, 1);
+    this.recipesChanged.next(this.recipes.slice());
   }
 }
