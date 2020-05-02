d37f21da747419a4f100832bea8a66fb638df306
JBar0 <baranowski.jakub97@gmail.com>
2020-05-02 21:38:41 +0200

    Auth probably broken

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,16 +6,24 @@ import {RecipeStartComponent} from './recipies/recipe-start/recipe-start.compone
 import {RecipeDetailComponent} from './recipies/recipe-detail/recipe-detail.component';
 import {RecipeEditComponent} from './recipies/recipe-edit/recipe-edit.component';
 import {RecipesResolverService} from './recipies/recipes-resolver.service';
+import {AuthComponent} from './auth/auth.component';
+import {AuthGuard} from './auth/auth.guard';
 
 const appRoutes: Routes = [
   {path: '', redirectTo: '/recipes', pathMatch: 'full'},
-  {path: 'recipes', component: RecipesComponent, children: [
+  {
+    path: 'recipes',
+    component: RecipesComponent,
+    canActivate: [AuthGuard],
+    children: [
       {path: '', component: RecipeStartComponent},
       {path: 'new', component: RecipeEditComponent},
       {path: ':id', component: RecipeDetailComponent, resolve: [RecipesResolverService]},
       {path: ':id/edit', component: RecipeEditComponent, resolve: [RecipesResolverService]}
-    ]},
-  {path: 'shopping-list', component: ShoppingListComponent}
+    ]
+  },
+  {path: 'shopping-list', component: ShoppingListComponent},
+  {path: 'auth', component: AuthComponent}
 ];
 
 @NgModule({
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,8 +1,9 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpParams} from '@angular/common/http';
 import {RecipeService} from '../recipies/recipe.service';
 import {RecipeModel} from '../recipies/recipe.model';
-import {map, tap} from 'rxjs/operators';
+import {exhaustMap, map, take, tap} from 'rxjs/operators';
+import {AuthService} from '../auth/auth.service';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +12,8 @@ export class DataStorageService {
   private url = 'https://ng-course-recipe-book-de1b1.firebaseio.com/recipes.json';
 
   constructor(private http: HttpClient,
-              private recipeService: RecipeService) {
+              private recipeService: RecipeService,
+              private authService: AuthService) {
   }
 
   storeRecipes() {
@@ -23,13 +25,15 @@ export class DataStorageService {
   }
 
   fetchRecipes() {
+
     return this.http
       .get<RecipeModel[]>(this.url)
-      .pipe(map(recipes => {
-        return recipes.map(recipe => {
-          return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []};
-        });
-      }),
+      .pipe(
+        map(recipes => {
+          return recipes.map(recipe => {
+            return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []};
+          });
+        }),
         tap(recipes => {
           this.recipeService.setRecipes(recipes);
         })
