d724f2d0057fb16bac6f0cecacf520ac2fed9295
JBar0 <baranowski.jakub97@gmail.com>
2020-05-02 22:58:13 +0200

    Alert component added dynamically

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -13,7 +13,9 @@ export class AuthGuard implements CanActivate {
               private router: Router) {
   }
 
-  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot)
+    : Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+
     return this.authService.user.pipe(
       take(1),
       map(user => {
